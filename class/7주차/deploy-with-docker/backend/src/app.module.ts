8201500e282264e1cbf04d85b40af462922043ee
KIMSEONGRAN <doll950904@gmail.com>
2022-06-24 17:00:45 +0900

    deploy

--- class/7주차/deploy-with-docker/backend/src/app.module.ts
+++ class/7주차/deploy-with-docker/backend/src/app.module.ts
@@ -32,7 +32,7 @@ import { FileModule } from './apis/file/file.module';
       port: 3306,
       username: 'root',
       password: 'root',
-      database: 'mypdocker03',
+      database: 'mydocker03',
       entities: [__dirname + '/apis/**/*.entity.*'], // 현재 경로(__dirname)에서 apis로 들어가서 그 안에 모든폴더에 들어가서(**) 모든 entity.ts로 끝나는 파일 연결하겠다.
       synchronize: true, // 동기화 시켜주겠다.
       logging: true, // 쿼리문에 대한 로그
